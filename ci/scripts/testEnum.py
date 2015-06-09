7a930b2d7637b4e4e400a2f24ad3dcb8fb39e5ca
pploegae <philippe.ploegaert@openvstorage.com>
2015-06-09 15:03:34 +0200

    Merge test -> stable

---
+++ ci/scripts/testEnum.py
@@ -0,0 +1,45 @@
+"""
+This plugin enumerates the tests in a format that can be copy pasted as input for running tests.
+"""
+from nose.plugins.base import Plugin
+
+
+class TestEnum(Plugin):
+    """
+    Nose plugin to enumerate tests
+    """
+    name = 'testEnum'
+    score = 400
+
+    def __init__(self):
+        Plugin.__init__(self)
+        self._tests = []
+
+    def options(self, parser, env):
+        Plugin.options(self, parser, env)
+
+    def configure(self, options, conf):
+        Plugin.configure(self, options, conf)
+        self.config = conf
+
+    def startTest(self, test):
+        testinfo = test.address()
+        import os
+
+        os.write(1, str(testinfo) + "\n")
+        testName = '%s:%s' % (testinfo[1], testinfo[2])
+        if testName not in self._tests:
+            self._tests.append(testName)
+        return None
+
+    def setOutputStream(self, stream):
+        class DummyStream(object):
+            writeln = flush = write = lambda self, *args: None
+
+        return DummyStream()
+
+    def finalize(self, result):
+        for testName in self._tests:
+            print testName
+        return None
+
