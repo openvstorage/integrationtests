52451299ce21a92d34b086b045853b9c23d84703
eduardBM <cubusbacau@yahoo.com>
2015-11-12 23:06:58 +0200

    Fix cinder ci

--- cinderci/dsvm-tempest-full/patches.sh
+++ cinderci/dsvm-tempest-full/patches.sh
@@ -2,20 +2,20 @@ sudo sed -i 's|/opt/stack/cinder/cinder/|/opt/stack/new/cinder/cinder/|g' /opt/O
 sudo sed -i 's|/opt/stack/new/nova/nova/virt/libvirt/volume.py|/opt/stack/new/nova/nova/virt/libvirt/volume/volume.py|g' /opt/OpenvStorage/ovs/extensions/hypervisor/mgmtcenters/management/openstack_mgmt.py
 
 echo "diff --git a/ovs/lib/disk.py b/ovs/lib/disk.py
-index 3e13d81..7c353fd 100644
+index 916be39..0a4f6e0 100644
 --- a/ovs/lib/disk.py
 +++ b/ovs/lib/disk.py
-@@ -69,7 +69,8 @@ class DiskController(object):
+@@ -68,7 +68,8 @@ class DiskController(object):
+             with Remote(storagerouter.ip, [Context, os]) as remote:
                  context = remote.Context()
                  devices = [device for device in context.list_devices(subsystem='block')
-                            if ('ID_TYPE' in device and device['ID_TYPE'] == 'disk')
--                           or ('DEVNAME' in device and 'nvme' in device['DEVNAME'])]
-+                           or ('DEVNAME' in device and 'nvme' in device['DEVNAME'])
+-                           if 'ID_TYPE' in device and device['ID_TYPE'] == 'disk']
++                           if 'ID_TYPE' in device and device['ID_TYPE'] == 'disk'
 +                           or (device['DEVTYPE'] in ('disk', 'partition') and device['DEVNAME'].startswith('/dev/vda'))]
                  for device in devices:
                      is_partition = device['DEVTYPE'] == 'partition'
                      device_path = device['DEVNAME']
-@@ -99,9 +100,10 @@ class DiskController(object):
+@@ -98,9 +99,10 @@ class DiskController(object):
                      for path_type in ['by-id', 'by-uuid']:
                          if path is not None:
                              break
@@ -29,5 +29,6 @@ index 3e13d81..7c353fd 100644
                      if path is None:
                          path = device_path
                      sectors = int(client.run('cat /sys/block/{0}/size'.format(device_name)))
+
 " | sudo tee /opt/OpenvStorage/patch_disk.diff
 sudo patch /opt/OpenvStorage/ovs/lib/disk.py /opt/OpenvStorage/patch_disk.diff
