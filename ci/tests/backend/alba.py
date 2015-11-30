29bba228e8c5850545c9efd7307d7c40f51da564
pploegaert <philippe.ploegaert@openvstorage.com>
2015-11-30 09:00:10 +0100

    license update

--- ci/tests/backend/alba.py
+++ ci/tests/backend/alba.py
@@ -1,10 +1,10 @@
-# Copyright 2014 iNuron NV
+# Copyright 2015 iNuron NV
 #
-# Licensed under the Open vStorage Non-Commercial License, Version 1.0 (the "License");
+# Licensed under the Open vStorage Modified Apache License (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.openvstorage.org/OVS_NON_COMMERCIAL
+#     http://www.openvstorage.org/license
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -44,6 +44,7 @@ def run(backend_name, action, params, json_output=True):
         cmd.append('--to-json')
     cmd.extend(params)
 
+    output = ''
     try:
         output = execute_command(' '.join(cmd))
         if json_output:
