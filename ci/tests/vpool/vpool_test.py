76f66a13aedea3ed169e69e4d0f1180fc8d86536
mnuca <marius.nuca@openvstorage.com>
2016-01-21 14:03:37 +0200

    OVS-2703 added the kill various services test also fixed the push to testrail issues from console as well as an issue with api adding a new testcase to the project

--- ci/tests/vpool/vpool_test.py
+++ ci/tests/vpool/vpool_test.py
@@ -91,3 +91,46 @@ def ovs_2263_verify_alba_namespace_cleanup_test():
         alba.delete_namespace(BACKEND_NAME, namespace['name'])
     result = alba.list_namespaces(BACKEND_NAME)
     assert len(result) == 0, "Expected no namespaces present on the {1} backend, found {2}".format(no_namespaces, BACKEND_NAME, len(result))
+
+
+def ovs_2703_kill_various_services():
+    """
+    Kill various services and see if they recover
+    %s
+    """ % general.get_function_name()
+
+    general.check_prereqs(testcase_number=3,
+                          tests_to_run=testsToRun)
+    issues_found = ''
+    api = Connection.get_connection()
+    vpool_list = api.get_component_by_name('vpools', VPOOL_NAME)
+    if not vpool_list:
+        generic.add_generic_vpool()
+
+    services_to_kill = [  # framework services
+                          'ovs-watcher-volumedriver',
+                          'ovs-watcher-framework',
+                          'ovs-webapp-api',
+                          'ovs-workers',
+                          # alba services
+                          'ovs-albaproxy',
+                          'alba-asdmanager']
+
+    for service in services_to_kill:
+        out, err = general.execute_command("initctl list | grep {0}".format(service))
+        if not err and len(out):
+            service_status = out.split(' ')[1][:-1]
+            service_proc_id = out.split(' ')[3][:-1]
+            if service_status not in 'start/running':
+                issues_found += 'Service {0} not found in running state\n'.format(service)
+            else:
+                out, err = general.execute_command("kill -9 {0}".format(service_proc_id))
+                time.sleep(5)
+                out, err = general.execute_command("initctl list | grep {0}".format(service))
+                if len(out) == 0:
+                    issues_found += 'Service {0} not found after kill command issued\n'.format(service)
+                else:
+                    if out.split(' ')[1][:-1] not in 'start/running':
+                        issues_found += 'Service {0} not found in running state after kill command issued\n'.format(service)
+
+    assert issues_found == '', "Followind issues where found with the services:\n{0}".format(issues_found)
