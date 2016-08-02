a2dd827709c9f373fe47e9b3be3c828143acbb34
pploegaert <philippe.ploegaert@openvstorage.com>
2016-08-02 16:04:27 +0200

    Removed obsolete scrubbing test

--- ci/tests/vdisk/vdisk_test.py
+++ ci/tests/vdisk/vdisk_test.py
@@ -18,13 +18,11 @@
 Virtual Disk testsuite
 """
 
-import os
 from ci.tests.general.general import General
 from ci.tests.general.general_vdisk import GeneralVDisk
 from ci.tests.general.general_vpool import GeneralVPool
 from ci.tests.general.logHandler import LogHandler
 from ovs.dal.hybrids.vdisk import VDisk
-from ovs.lib.scheduledtask import ScheduledTaskController
 
 
 class TestVDisk(object):
@@ -35,40 +33,6 @@ class TestVDisk(object):
 
     vpool_name = General.get_config().get("vpool", "name")
     assert vpool_name, 'vPool name required in autotest.cfg file'
-
-    @staticmethod
-    def ovs_3700_validate_test():
-        """
-        Validate something test
-        """
-        def _get_scrubber_log_size():
-            scrubber_log_name = '/var/log/upstart/ovs-scrubber.log'
-            if os.path.exists(scrubber_log_name):
-                return os.stat(scrubber_log_name).st_size
-            return 0
-
-        loop = 'loop0'
-        vpool = GeneralVPool.get_vpool_by_name(TestVDisk.vpool_name)
-        vdisk = GeneralVDisk.create_volume(size=2, vpool=vpool, name='ovs-3700-disk', loop_device=loop, wait=True)
-
-        GeneralVDisk.create_snapshot(vdisk=vdisk, snapshot_name='snap0')
-        GeneralVDisk.generate_hash_file(full_name='/mnt/{0}/{1}_{2}.txt'.format(loop, vdisk.name, '1'), size=512)
-
-        GeneralVDisk.create_snapshot(vdisk=vdisk, snapshot_name='snap1')
-        GeneralVDisk.generate_hash_file(full_name='/mnt/{0}/{1}_{2}.txt'.format(loop, vdisk.name, '2'), size=512)
-
-        GeneralVDisk.delete_snapshot(disk=vdisk, snapshot_name='snap1')
-
-        GeneralVDisk.generate_hash_file(full_name='/mnt/{0}/{1}_{2}.txt'.format(loop, vdisk.name, '3'), size=512)
-        GeneralVDisk.create_snapshot(vdisk=vdisk, snapshot_name='snap2')
-
-        pre_scrubber_logsize = _get_scrubber_log_size()
-        ScheduledTaskController.gather_scrub_work()
-        post_scrubber_logsize = _get_scrubber_log_size()
-
-        GeneralVDisk.delete_volume(vdisk=vdisk, vpool=vpool, loop_device=loop)
-
-        assert post_scrubber_logsize > pre_scrubber_logsize, "Scrubber actions were not logged!"
 
     @staticmethod
     def ovs_3756_metadata_size_test():
